5fd487ec7f089d3705cd38f49eb64084089c9242
PcKaguya <124661732+PcKaguya@users.noreply.github.com>
2025-11-09 12:41:56 +1000

    Delete Leaked Info/webhook.my directory

--- Leaked Info/webhook.my/backend/404.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Location: https://webhook.my');
-exit;
-?>
--- Leaked Info/webhook.my/backend/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'webhookURL' => 'https://discord.com/api/webhooks/1328460928722927678/gB0W3B5EUsn_ulWoLS7usUV5wNMy-pyBpQYRekbNfIyBz_iKfWni2kc9slHhaCXBDnC3'
-];
